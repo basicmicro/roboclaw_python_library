047857b56b623d3fee94c2bab680134b66e7e916
Philip Smith <30378846+c4glenn@users.noreply.github.com>
2023-10-28 01:37:30 -0400

    fix typo in robowclaw3

--- roboclaw_python/roboclaw_3.py
+++ roboclaw_python/roboclaw_3.py
@@ -846,7 +846,7 @@ class Roboclaw:
 		return (0,0,0)
 
 	def SpeedAccelM1M2_2(self,address,accel1,speed1,accel2,speed2):
-		return self._write4S44S4(address,self.Cmd.MIXEDSPEED2ACCEL,accel,speed1,accel2,speed2)
+		return self._write4S44S4(address,self.Cmd.MIXEDSPEED2ACCEL,accel1,speed1,accel2,speed2)
 
 	def SpeedAccelDistanceM1M2_2(self,address,accel1,speed1,distance1,accel2,speed2,distance2,buffer):
 		return self._write4S444S441(address,self.Cmd.MIXEDSPEED2ACCELDIST,accel1,speed1,distance1,accel2,speed2,distance2,buffer)
